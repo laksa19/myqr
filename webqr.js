48b72221108aa7d30acac71352a69bdfae5c075a
Laksamadi Guko <elru.lakusa@gmail.com>
2018-08-15 23:47:58 +0700

    Update webqr.js

--- webqr.js
+++ webqr.js
@@ -12,9 +12,9 @@ var v=null;
 
 var imghtml='<div id="qrfile"><canvas id="out-canvas" width="320" height="240"></canvas>'+
     '<div id="imghelp">drag and drop a QRCode here'+
-	'<br>or select a file'+
-	'<input type="file" onchange="handleFiles(this.files)"/>'+
-	'</div>'+
+    '<br>or select a file'+
+    '<input type="file" onchange="handleFiles(this.files)"/>'+
+    '</div>'+
 '</div>';
 
 var vidhtml = '<video id="v" autoplay></video>';
@@ -36,30 +36,30 @@ function drop(e) {
   var files = dt.files;
   if(files.length>0)
   {
-	handleFiles(files);
+    handleFiles(files);
   }
   else
   if(dt.getData('URL'))
   {
-	qrcode.decode(dt.getData('URL'));
+    qrcode.decode(dt.getData('URL'));
   }
 }
 
 function handleFiles(f)
 {
-	var o=[];
-	
-	for(var i =0;i<f.length;i++)
-	{
+    var o=[];
+    
+    for(var i =0;i<f.length;i++)
+    {
         var reader = new FileReader();
         reader.onload = (function(theFile) {
         return function(e) {
             gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
 
-			qrcode.decode(e.target.result);
+            qrcode.decode(e.target.result);
         };
         })(f[i]);
-        reader.readAsDataURL(f[i]);	
+        reader.readAsDataURL(f[i]); 
     }
 }
 
@@ -107,9 +107,9 @@ function read(a)
     if(a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
         html+="<a target='_blank' href='"+a+"'>"+a+"</a><br>";
     html+="<b>"+htmlEntities(a)+"</b><br><br>";
-    document.getElementById("result").innerHTML=html;
-    window.location=a;
-}	
+    //document.getElementById("result").innerHTML=html;
+    window.location = a;
+}   
 
 function isCanvasSupported(){
   var elem = document.createElement('canvas');
@@ -124,7 +124,7 @@ function success(stream)
     gUM=true;
     setTimeout(captureToCanvas, 500);
 }
-		
+        
 function error(error)
 {
     gUM=false;
@@ -133,57 +133,57 @@ function error(error)
 
 function load()
 {
-	if(isCanvasSupported() && window.File && window.FileReader)
-	{
-		initCanvas(800, 600);
-		qrcode.callback = read;
-		document.getElementById("mainbody").style.display="inline";
+    if(isCanvasSupported() && window.File && window.FileReader)
+    {
+        initCanvas(800, 600);
+        qrcode.callback = read;
+        document.getElementById("mainbody").style.display="inline";
         setwebcam();
-	}
-	else
-	{
-		document.getElementById("mainbody").style.display="inline";
-		document.getElementById("mainbody").innerHTML='<p id="mp1">QR code scanner for HTML5 capable browsers</p><br>'+
+    }
+    else
+    {
+        document.getElementById("mainbody").style.display="inline";
+        document.getElementById("mainbody").innerHTML='<p id="mp1">QR code scanner for HTML5 capable browsers</p><br>'+
         '<br><p id="mp2">sorry your browser is not supported</p><br><br>'+
         '<p id="mp1">try <a href="http://www.mozilla.com/firefox"><img src="firefox.png"/></a> or <a href="http://chrome.google.com"><img src="chrome_logo.gif"/></a> or <a href="http://www.opera.com"><img src="Opera-logo.png"/></a></p>';
-	}
+    }
 }
 
 function setwebcam()
 {
-	
-	var options = true;
-	if(navigator.mediaDevices && navigator.mediaDevices.enumerateDevices)
-	{
-		try{
-			navigator.mediaDevices.enumerateDevices()
-			.then(function(devices) {
-			  devices.forEach(function(device) {
-				if (device.kind === 'videoinput') {
-				  if(device.label.toLowerCase().search("back") >-1)
-					options={'deviceId': {'exact':device.deviceId}, 'facingMode':'environment'} ;
-				}
-				console.log(device.kind + ": " + device.label +" id = " + device.deviceId);
-			  });
-			  setwebcam2(options);
-			});
-		}
-		catch(e)
-		{
-			console.log(e);
-		}
-	}
-	else{
-		console.log("no navigator.mediaDevices.enumerateDevices" );
-		setwebcam2(options);
-	}
-	
+    
+    var options = true;
+    if(navigator.mediaDevices && navigator.mediaDevices.enumerateDevices)
+    {
+        try{
+            navigator.mediaDevices.enumerateDevices()
+            .then(function(devices) {
+              devices.forEach(function(device) {
+                if (device.kind === 'videoinput') {
+                  if(device.label.toLowerCase().search("back") >-1)
+                    options={'deviceId': {'exact':device.deviceId}, 'facingMode':'environment'} ;
+                }
+                console.log(device.kind + ": " + device.label +" id = " + device.deviceId);
+              });
+              setwebcam2(options);
+            });
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
+    }
+    else{
+        console.log("no navigator.mediaDevices.enumerateDevices" );
+        setwebcam2(options);
+    }
+    
 }
 
 function setwebcam2(options)
 {
-	console.log(options);
-	document.getElementById("result").innerHTML="- scanning -";
+    console.log(options);
+    //document.getElementById("result").innerHTML="- scanning -";
     if(stype==1)
     {
         setTimeout(captureToCanvas, 500);    
@@ -205,18 +205,37 @@ function setwebcam2(options)
     }
     else
     if(n.getUserMedia)
-	{
-		webkit=true;
+    {
+        webkit=true;
         n.getUserMedia({video: options, audio: false}, success, error);
-	}
+    }
     else
     if(n.webkitGetUserMedia)
     {
         webkit=true;
         n.webkitGetUserMedia({video:options, audio: false}, success, error);
     }
-
+/*
+    document.getElementById("qrimg").style.opacity=0.2;
+    document.getElementById("webcamimg").style.opacity=1.0;
+*/
     stype=1;
     setTimeout(captureToCanvas, 500);
 }
 
+function setimg()
+{
+    document.getElementById("result").innerHTML="";
+    if(stype==2)
+        return;
+    document.getElementById("outdiv").innerHTML = imghtml;
+    //document.getElementById("qrimg").src="qrimg.png";
+    //document.getElementById("webcamimg").src="webcam2.png";
+    document.getElementById("qrimg").style.opacity=1.0;
+    document.getElementById("webcamimg").style.opacity=0.2;
+    var qrfile = document.getElementById("qrfile");
+    qrfile.addEventListener("dragenter", dragenter, false);  
+    qrfile.addEventListener("dragover", dragover, false);  
+    qrfile.addEventListener("drop", drop, false);
+    stype=2;
+}
